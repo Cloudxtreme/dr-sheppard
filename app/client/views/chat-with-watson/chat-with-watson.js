96ed1f0a0515d2907b4f6d55517bb379832aa617
maxim <uzumaxy@gmail.com>
2015-10-16 12:44:16 +0300

    Added `fontawesome` package.

--- app/client/views/chat-with-watson/chat-with-watson.js
+++ app/client/views/chat-with-watson/chat-with-watson.js
@@ -2,7 +2,9 @@ Template.ChatWithWatson.events({
   'submit form[data-action="ask-watson"]': (e, tpl) => {
     e.preventDefault();
     const question = tpl.find('input#question').value;
+    tpl.loading.set(true);
     Meteor.call('askWatson', question, (err, answer) => {
+      tpl.loading.set(false);
       if (err) {
         const msg = err.reason || err.text || 'Unknown error!';
         alert(msg);
@@ -17,10 +19,19 @@ Template.ChatWithWatson.events({
 Template.ChatWithWatson.helpers({
   answer: () => {
     return Template.instance().answer.get();
+  },
+  loading: () => {
+    return Template.instance().loading.get();
   }
 });
 
 
 Template.ChatWithWatson.onCreated(function () {
   this.answer = new ReactiveVar();
+  this.loading = new ReactiveVar();
+});
+
+
+Template.ChatWithWatson.onRendered(function () {
+  this.$('input').addClear();
 });
